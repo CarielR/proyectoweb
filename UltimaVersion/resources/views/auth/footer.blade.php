14b244fc68662120abd57056d31e2c1ab12994e1
CARLOS EDUARDO LÓPEZ CANDELEJO <celopez8@espe.edu.ec>
2023-06-24 21:01:55 -0500

    Ultima version con admin modificado

---
+++ UltimaVersion/resources/views/auth/footer.blade.php
@@ -0,0 +1,17 @@
+@section('footer')
+  <footer id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="copyright">
+            &copy; Copyright <strong>Analytika Women</strong>. All Rights Reserved
+          </div>
+          <div class="credits">        
+            <a href="#Home">Analytika Women</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- #footer -->
+  @endsection
---
+++ UltimaVersion/resources/views/dashboard.blade.php
@@ -0,0 +1,18 @@
+
+@extends('adminlte::page')
+
+@section('title', 'Analytika Women')
+
+@section('content_header')
+    <h1>Analytika Women</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop

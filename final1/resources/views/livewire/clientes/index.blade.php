57e0eb101eccc16249d9cd1c7f15f39f23e4432d
Kgabriel29 <alejovaca18@gmail.com>
2023-07-11 20:20:51 -0500

    SidebarCrudClientes

--- final1/resources/views/livewire/clientes/index.blade.php
+++ final1/resources/views/livewire/clientes/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
-@section('content')
+
 <div class="container-fluid">
+    <x-side-bar/>
     <div class="row justify-content-center">
         <div class="col-md-12">
             @livewire('clientes')
         </div>     
     </div>   
 </div>
-@endsection
